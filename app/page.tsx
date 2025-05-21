b667672baa37696b2d2d19b36f695441d398499b
Garbarsch <75129820+Garbarsch@users.noreply.github.com>
2025-05-21 17:49:37 +0200

    DoneReal

--- app/page.tsx
+++ app/page.tsx
@@ -348,6 +348,7 @@ export default function Home() {
             {/* MAIN UI */}
             <div className="h-full p-6 flex flex-col space-y-6 overflow-auto">
               <h1 className="text-2xl font-bold"> </h1>
+              <h1 className="text-2xl font-bold"> </h1>
 
               <div>
                 <h2 className="mb-1">Productivity</h2>
